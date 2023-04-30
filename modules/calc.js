e53e14e0ff09acface66fb0cf228f759b74b5b02
DosX <117774904+DosX-dev@users.noreply.github.com>
2023-04-30 11:20:25 +0300

    Create calc.js

---
+++ modules/calc.js
@@ -0,0 +1,17 @@
+function Calculate(Ex) {
+	var allowedChars = '0123456789+-*/(). ';
+	for (var i = 0; i < Ex.length; i++) {
+		if (allowedChars.indexOf(Ex[i]) === -1) {
+			throw new Error('Invalid char.');
+		}
+	}
+
+	try {
+		var result = eval(Ex);
+		if (result == undefined || result == NaN) {
+			throw new Error();
+		}
+		return result;
+	}
+	catch (error) {throw new Error('Invalid expression.');}
+}
